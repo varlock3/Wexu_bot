096b4a2628d24a781e65dd3aedd1142c60f24dee
ImZa1D <43499446+ImZa1D@users.noreply.github.com>
2018-09-27 20:19:30 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,28 @@ client.on('message', msg => {
 });
 
 
+ client.on('ready', function(){	
+    var ms = 10000 ;	
+    var setGame = ['%help','%دعوه'];	
+    var i = -1;	
+    var j = 0;	
+    setInterval(function (){	
+        if( i == -1 ){	
+j = 1;	
+       }	
+        if( i == (setGame.length)-1 ){	
+            j = -1;	
+      }	
+       i = i+j;	
+              client.user.setGame(setGame[i],`https://www.twitch.tv/jokar_999`);
+}, ms);	
+	
+});
+
+
+
+
+
 
 client.on('message', message => {
             if (message.content.startsWith("Rules")) {
