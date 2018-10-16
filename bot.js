631870dd2fc643576bfc07701b39379d76054770
ImZa1D <43499446+ImZa1D@users.noreply.github.com>
2018-10-16 18:32:30 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -110,7 +110,7 @@ client.on('message', message => {
 
 
 client.on('message', message => {
-var prefix = "!";
+var prefix = "-";
 
     if (message.author.id === client.user.id) return;
     if (message.guild) {
