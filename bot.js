b3780c65e8ae239ea4f08cdc8c6f0da206a51e90
ImZa1D <43499446+ImZa1D@users.noreply.github.com>
2018-10-20 12:23:53 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1280,24 +1280,6 @@ client.on('message', message => {
 });
 
 
-// Server list --------------------------------------------------------------------------------
-
-
-client.on('message', message => {
-  if(message.author.bot) return;
-    if(message.content === prefix + 'gm') {
-      var sg = client.guilds.filter(o => o.memberCount > 100).map(e => e.name).join('\n')
-      var gl = client.guilds.filter(g => g.memberCount < 100).map(n => n.name).join('\n')
-      var gm = new Discord.RichEmbed()
-      .setDescription('- قائمة اعضاء السيرفرات')
-      .setColor('RANDOM')
-      .addField('- قائمة السيرفرات التي تملك فوق المئة عضو', sg || "0")
-      .addField('- قائمة السيرفرات التي تملك اقل من مئة عضو', gl || "0")
-      .setFooter(Guilds: ${client.guilds.size}, Users: ${client.users.size}, Channels: ${client.channels.size}.)
-      message.channel.send(gm);
-    }
-});
-
 
 // AUTO ROLE --------------------------------------------------------------------------------
 
