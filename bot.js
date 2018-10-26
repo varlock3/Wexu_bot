3bc0ee6b573009fe6a7fd53069bd95f4d82c6003
ImZa1D <43499446+ImZa1D@users.noreply.github.com>
2018-10-27 01:42:35 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,30 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const fs = require("fs");
 var prefix = "?"
-
+const db = require('quick.db');  
+const giphy = require('giphy-api')();    
+const googl = require('goo.gl');  
+const translate = require('google-translate-api');   
+const fs = require("fs"); 
+const canvas = require("canvas");
+const getYoutubeID = require('get-youtube-id'); 
+const moment = require("moment");  
+const { Client, Util } = require('discord.js');  
+const UserBlocked = new Set(); 
+const jimp = require('jimp');   
+const math = require('math-expression-evaluator'); 
+const stripIndents = require('common-tags').stripIndents;
+const figlet = require('figlet');
+const google = require('google-it'); 
+const queue = new Map(); 
+const zalgo = require('zalgolize');   
+const fetchVideoInfo = require('youtube-info');
+const YouTube = require('simple-youtube-api');
+const ytdl = require('ytdl-core');
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+const sql = require("sqlite");
+ const dateFormat = require('dateformat'); 
+ const pretty = require('pretty-ms') 
 
 
 client.on('ready', () => {
