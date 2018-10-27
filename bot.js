321c53c18fa27294b46aeeedbc3c135484687fba
ImZa1D <43499446+ImZa1D@users.noreply.github.com>
2018-10-27 12:25:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1476,6 +1476,32 @@ mentionned.send(` :credit_card: | Transfer Receipt \`\`\`You have received ${arg
 });
 
 
+///Member count --------------------
+
+
+
+client.on('message',async msg => {
+  var p = "?";
+  if(msg.content.startsWith(p + "mem")) {
+  if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **go play minecraft**');
+  if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');
+  msg.guild.createChannel(`يتم تحضير الروم :[]` , 'voice').then(time => {
+    time.overwritePermissions(msg.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+  setInterval(() => {
+      var currentTime = new Date(),
+Year = currentTime.getFullYear(),
+Month = currentTime.getMonth() + 1,
+Dat = currentTime.getDate()
+      time.setName(`Members : ◤ → ${client.users.size} ← ◢`);
+ },1000);
+  });
+  }
+ 
+});
+
 
 
 // TOKEN --------------------------------------------------------------------------------
