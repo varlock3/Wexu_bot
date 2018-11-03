252412c0d8210a705b2ebd966eb764e0ddfaa7a1
ImZa1D <43499446+ImZa1D@users.noreply.github.com>
2018-11-03 18:20:41 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,28 +35,57 @@ client.on('ready', () => {
 
 // BC --------------------------------------------------------------------------------
 
-client.on('message', message => {
-    if (message.author.id === client.user.id) return;
-    if (message.guild) {
-   let embed = new Discord.RichEmbed()
-    let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith(prefix + "bc")) {
-    if (!args[1]) {
-return;
-}
-        message.guild.members.forEach(m => {
-   if(!message.member.hasPermission('ADMINISTRATOR')) return;
-            var bc = new Discord.RichEmbed()
-            .addField(' » الرسالة : ', args)
-            .setColor('#ff0000')
-            // m.send(`[${m}]`);
-            m.send(`${m}`,{embed: bc});
+
+client.on('message',async message => {
+
+  if(message.author.bot || message.channel.type === 'dm') return;
+
+  let args = message.content.split(' ');
+
+  if(args[0] === `${prefix}bc`) {
+
+    if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send('- **أنت لا تملك الصلاحيات اللازمة لأستخدام هذا الأمر**');
+
+    if(!args[1]) return message.channel.send('- **يجب عليك كتابة الرسالة بعد الأمر**');
+
+  
+
+    let msgCount = 0;
+
+    let errorCount = 0;
+
+    let successCount = 0;
+
+    message.channel.send(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`).then(msg => {
+
+      message.guild.members.forEach(g => {
+
+        g.send(args.slice(1).join(' ')).then(() => {
+
+          successCount++;
+
+          msgCount++;
+
+          msg.edit(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`);
+
+        }).catch(e => {
+
+          errorCount++;
+
+          msgCount++;
+
+          msg.edit(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`);
+
         });
-    }
-    } else {
-        return;
-    }
+
+      });
+
+    });
+
+  }
+
 });
+
 
 //Mute ----------------------------------------------------------------------------------
 
