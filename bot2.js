4e1c64cdb830bb09a7865f90ba95db43ad5a4c57
ImZa1D <43499446+ImZa1D@users.noreply.github.com>
2018-11-13 20:27:22 +0400

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -1248,29 +1248,6 @@ client.on("guildMemberAdd", member => {
 })
 
 
-/// !users Room 
-
-client.on('message',async msg => {
-  var p = "!";
-  if(msg.content.startsWith(p + "user")) {
-  if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **go play minecraft**');
-  if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');
-  msg.guild.createChannel(`يتم تحضير الروم :[]` , 'voice').then(time => {
-    time.overwritePermissions(msg.guild.id, {
-      CONNECT: false,
-      SPEAK: false
-    });
-  setInterval(() => {
-      var currentTime = new Date(),
-Year = currentTime.getFullYear(),
-Month = currentTime.getMonth() + 1,
-Dat = currentTime.getDate()
-      time.setName(`Members : ◤ → ${member.guild.memberCount} ← ◢`);
- },1000);
-  });
-  }
- 
-});
 
 
 
